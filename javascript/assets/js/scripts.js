c2c06ddd9f401b5b9f67b109fbf2fbf11eebad21
diegomota2374 <diegomota2374@gmail.com>
2022-06-04 13:14:53 -0300

    modificações no exercicio

--- javascript/assets/js/scripts.js
+++ javascript/assets/js/scripts.js
@@ -1,34 +1,34 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
-var botomIncrement = document.getElementsByClassName("adicionar");
-var botomDecrement = document.getElementsByClassName("subtrair");
-var currentNumber = 0;
+var currentNumberWrapper = document.getElementById('currentNumber')
+var botomIncrement = document.getElementsByClassName("adicionar")
+var botomDecrement = document.getElementsByClassName("subtrair")
+var currentNumber = 0
 
 botomIncrement[0].addEventListener('click',()=>{   
     
     if(currentNumber < 10){
-        currentNumber++;
+        currentNumber++
         currentNumberWrapper.innerHTML = currentNumber;
-        botomIncrement[0].style.backgroundColor="rgb(226, 214, 241)";
-        botomDecrement[0].style.backgroundColor="rgb(226, 214, 241)";
+        botomIncrement[0].style.backgroundColor="rgb(226, 214, 241)"
+        botomDecrement[0].style.backgroundColor="rgb(226, 214, 241)"
     }else{
-        botomIncrement.disabled = true;
-        botomIncrement[0].style.backgroundColor="rgb(125 118 133)"; 
+        botomIncrement.disabled = true
+        botomIncrement[0].style.backgroundColor="rgb(125 118 133)"
     }
-});
+})
 
 botomDecrement[0].addEventListener('click',()=>{ 
 
     if(currentNumber > 0){
-        currentNumber--;
-        currentNumberWrapper.innerHTML = currentNumber;
-        botomDecrement[0].style.backgroundColor="rgb(226, 214, 241)";
-        botomIncrement[0].style.backgroundColor="rgb(226, 214, 241)";
+        currentNumber--
+        currentNumberWrapper.innerHTML = currentNumber
+        botomDecrement[0].style.backgroundColor="rgb(226, 214, 241)"
+        botomIncrement[0].style.backgroundColor="rgb(226, 214, 241)"
     }else{
-        botomDecrement.disabled = true;
-        botomDecrement[0].style.backgroundColor="rgb(125 118 133)"; 
+        botomDecrement.disabled = true
+        botomDecrement[0].style.backgroundColor="rgb(125 118 133)"
     }
     
-});
+})
 
 
 
